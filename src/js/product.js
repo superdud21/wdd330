ffaab488b24180530c2d24db6140cf2ea6d2fbac
Caleb Cameron <36425378+superdud21@users.noreply.github.com>
2025-05-22 20:52:20 -0400

    Completed Team Activity 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,16 @@
-import { getLocalStorage, setLocalStorage } from './utils.mjs';
+import { getParam } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData('tents');
+const productId = getParam('product');
 
-function addProductToCart(product) {
-  const cart = getLocalStorage('so-cart') || [];
-  cart.push(product);
-  setLocalStorage('so-cart', cart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+console.log(dataSource.findProductById(productId));
 
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+if (productId) {
+  const dataSource = new ProductData('tents');
+  const product = new ProductDetails(productId, dataSource);
+  product.init();
+} else {
+  console.error('Missing product ID in query string');
+}
